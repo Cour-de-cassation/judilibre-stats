801c469a332e26700c07a143a1143e3f49bd39d9
antoinefa <fabien.antoine@m4x.org>
2021-10-20 03:17:16 +0200

    add dsfr Tile

---
+++ src/lib/dsfr/Tile.svelte
@@ -0,0 +1,13 @@
+<div class="fr-tile">
+    <div class="fr-tile__body">
+        <h4 class="fr-tile__title">
+            <slot name="title"/>
+        </h4>
+        <p class="rf-tile__desc fr-mt-0 fr-mb-0">
+            <slot name="desc"/>
+        </p>
+    </div>
+</div>
+
+
+

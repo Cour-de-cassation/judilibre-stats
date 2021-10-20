8646046d1968137aab8c8d2f867ffa874c6ce0c9
antoinefa <fabien.antoine@m4x.org>
2021-10-20 03:17:02 +0200

    add dsfr Footer

---
+++ src/lib/dsfr/Footer.svelte
@@ -0,0 +1,36 @@
+<footer class="fr-footer fr-mt-4w" role="contentinfo" id="footer">
+    <div class="fr-container">
+        <div class="fr-footer__body">
+            <div class="fr-footer__brand fr-enlarge-link">
+                <a href="/" title="Retour à l’accueil">
+                    <p class="fr-logo" title="république française">
+                        Cour de
+                        <br>cassation
+                    </p>
+                </a>
+            </div>
+            <div class="fr-footer__content">
+                <ul class="fr-footer__content-list">
+                    <li class="fr-footer__content-item">
+                        <a class="fr-footer__content-link" href="https://www.courdecassation.fr">Cour de cassation</a>
+                    </li>
+                    <li class="fr-footer__content-item">
+                        <a class="fr-footer__content-link" href="https://www.courdecassation.fr/recherche-judilibre">Recherche Judilibre</a>
+                    </li>
+                    <li class="fr-footer__content-item">
+                        <a class="fr-footer__content-link" href="https://github.com/Cour-de-cassation">Codes source</a>
+                    </li>
+                    <li class="fr-footer__content-item">
+                        <a class="fr-footer__content-link" href="https://piste.gouv.fr/">piste.gouv.fr</a>
+                    </li>
+                    <li class="fr-footer__content-item">
+                        <a class="fr-footer__content-link" href="https://api.gouv.fr/les-api/api-judilibre">api.gouv.fr</a>
+                    </li>
+                    <li class="fr-footer__content-item">
+                        <a class="fr-footer__content-link" href="https://data.gouv.fr">data.gouv.fr</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>

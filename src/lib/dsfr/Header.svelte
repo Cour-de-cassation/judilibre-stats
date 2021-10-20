00d4d9027988db608511748e7d1ccd8ea983d6b6
antoinefa <fabien.antoine@m4x.org>
2021-10-20 03:16:45 +0200

    add dsfr Header

---
+++ src/lib/dsfr/Header.svelte
@@ -0,0 +1,31 @@
+<header role="banner" class="fr-header">
+    <div class="fr-header__body">
+        <div class="fr-container">
+            <div class="fr-header__body-row">
+                <div class="fr-header__brand fr-enlarge-link">
+                    <div class="fr-header__brand-top">
+                        <div class="fr-header__logo">
+                            <p class="fr-logo">
+                                Cour de
+                                <br>cassation
+                            </p>
+                        </div>
+                    </div>
+                    <div class="fr-header__service">
+                        <a href="/" title="Accueil - [À MODIFIER | Nom du site / service]">
+                          <p class="fr-header__service-title">API Judilibre</p>
+                        </a>
+                        <p class="fr-header__service-tagline">Statistiques de consultation</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<style>
+</style>
